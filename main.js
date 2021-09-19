4d72a46dcd6d4eaec0893f07ab390c500033dfcb
dwikinuridhuha <dwiki.nuridhuha@gmail.com>
2021-09-19 09:41:32 +0700

    staircase 1

--- main.js
+++ main.js
@@ -1,54 +1,22 @@
-// const arrayNumber = [-1, -4, 0, 1, 2, 3];
-const arrayNumber = [-4, 3, -9, 0, 4, 1];
-// const arrayNumber = [1, 2, 3, -1, -2, -3, 0, 0];
+const masukanN = 7;
 
-const ActionType = {
-    minus: "minus",
-    zero: "zero",
-    plus: "plus"
-};
-
-const cekFilter = (nums, type) => {
-    if (type === ActionType.minus) {
-        return nums.filter(ar => {
-            if (ar < 0) {
-                return ar;
+function staircase(n) {
+    for (let i = n; i > 0; i--) {
+        for (let j = 0; j <= i; j++) {
+            if (j === i) {
+                for (let k = 0; k <= (n - j); k++) {
+                    if (k === (n - j)) {
+                        process.stdout.write("#" + "\n");
+                    }
+                    else {
+                        process.stdout.write("#");
+                    }
+                }
+            } else {
+                process.stdout.write(" ");
             }
-
-        });
+        }
     }
-
-    if (type === ActionType.zero) {
-        return nums.filter(ar => {
-            if (ar === 0) {
-                return ar + 1;
-            }
-        });
-    }
-
-    if (type === ActionType.plus) {
-        return nums.filter(ar => {
-            if (ar > 0) {
-                return ar;
-            }
-        });
-    }
-};
-
-function plusMinus(arr) {
-    let nilaiArray = [];
-
-    const plus = cekFilter(arr, ActionType.plus);
-    const minus = cekFilter(arr, ActionType.minus);
-    const zero = cekFilter(arr, ActionType.zero);
-
-    nilaiArray[0] = (plus.length / arr.length).toFixed(6);
-    nilaiArray[1] = (minus.length / arr.length).toFixed(6);
-    nilaiArray[2] = (zero.length / arr.length).toFixed(6);
-
-    nilaiArray.forEach(ar => {
-        console.log(ar);
-    })
 }
 
-plusMinus(arrayNumber);
+staircase(masukanN);

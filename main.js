bc5a6812158ffe8cc9fb679d96893528956c707c
dwikinuridhuha <dwiki.nuridhuha@gmail.com>
2021-09-19 10:02:06 +0700

    staircase 2

--- main.js
+++ main.js
@@ -1,21 +1,8 @@
 const masukanN = 7;
 
 function staircase(n) {
-    for (let i = n; i > 0; i--) {
-        for (let j = 0; j <= i; j++) {
-            if (j === i) {
-                for (let k = 0; k <= (n - j); k++) {
-                    if (k === (n - j)) {
-                        process.stdout.write("#" + "\n");
-                    }
-                    else {
-                        process.stdout.write("#");
-                    }
-                }
-            } else {
-                process.stdout.write(" ");
-            }
-        }
+    for (let i = 1; i <= n; i++) {
+        console.log(" ".repeat(n - i) + "#".repeat(i))
     }
 }
 
